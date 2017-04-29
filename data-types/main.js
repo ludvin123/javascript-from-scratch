395a32a9700b482345113eecd94a3601e6e84f70
Alumno10 S304 <a10s304@campustls.local>
2017-04-29 13:57:57 -0500

    clase elizabeth

---
+++ data-types/main.js
@@ -0,0 +1,31 @@
+//ESLint
+
+var isBoolean = true;
+var isString = "Cadena de Texto";
+var isNumber = 3322;
+
+
+var isObject = {  // Objeto Literal
+	name: "Edmundo",
+	lastname: "Acosta",
+};
+
+
+var isArray = ['Edmundo', 'Bruno', 'Eli', 'Paolo'];
+
+var isFunction = function() {
+	return "Hola soy una funcion"
+}
+
+
+
+
+
+
+
+
+console.log(isBoolean, typeof isBoolean);
+console.log(isString, typeof isString);
+console.log(isNumber, typeof isNumber);
+console.log(isBoolean, typeof isBoolean);
+console.log(isFunction, typeof isFunction);
